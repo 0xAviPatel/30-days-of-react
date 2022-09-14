c43e671644191ccb707d0944000da2776b3064f8
Avi Patel <avip3513@gmail.com>
2022-09-14 08:27:58 +0530

    Day 12

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [values, getValues] = useState("");
+  const [values, getValues] = useState({
+    firstName: "",
+    lastName: "",
+    country: "",
+  });
+
+  // const handleChange = (e) => {
+  //   // const val = e.target.value;
+  //   // getValues(val);
+
+  // };
 
   const handleChange = (e) => {
-    const val = e.target.value;
-    getValues(val);
-    console.log(val)
+    const { name, value } = e.target;
+    getValues({
+      [name]: value,
+    });
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  const { firstName, lastName, country } = values;
   return (
     <div>
-      <input placeholder="Enter your Name" onChange={handleChange} />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Enter First Name"
+          value={firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter Last Name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter Country"
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
