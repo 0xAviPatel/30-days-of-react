616373df9d1f8f802f8312d5833cfc1014d75d17
Avi Patel <avip3513@gmail.com>
2022-09-14 07:54:22 +0530

    Day 12

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React, { useState } from "react";
-import LoggedIn from "./component/LoggedIn";
-import LoginButton from "./component/LoginButton";
 
 const App = () => {
-  // const numbers = [1, 2, 3, 4, 5];
+  const [values, getValues] = useState("");
 
-  // const [value, setValue] = useState(0);
-
-  // const [decrease, setDecrease] = useState(value);
-
-  // const changeNum = () => {
-  //   setValue(value + 1);
-  // };
-
-  // const decreaseNum = () => {
-  //   setDecrease(decrease - 1);
-  // };
-
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const Data = {
-    welcome: "Hello",
-    title: "Getting started with react",
-    subtitle: "learning ternary operator",
-    firstName: "Avi",
-    lastName: "Patel",
-    date: new Date().getDate(),
+  const handleChange = (e) => {
+    const val = e.target.value;
+    getValues(val);
+    console.log(val)
   };
-
-  // let status;
-  // let Text;
-
-  const onLoggedIn=()=>{
-    setLoggedIn(true)
-  }
-  // loggedIn
-  //   ? (Text = <h1>You are logged in</h1>)
-  //   : (Text = <h1>You are not logged in</h1>);
-
-
   return (
     <div>
-      {/* <button onClick={changeNum}>You have clicked {value} times</button>
-      <button onClick={decreaseNum}>You have clicked {decrease} times</button> */}
-      <LoggedIn data={Data} />
-      {/* {status} */}
-      <LoginButton text={loggedIn ? 'You are logged in' : 'You are not logged in'} onClick={onLoggedIn} />
-      
-      {!loggedIn && (<p>Please log in</p>)}
+      <input placeholder="Enter your Name" onChange={handleChange} />
     </div>
   );
 };
