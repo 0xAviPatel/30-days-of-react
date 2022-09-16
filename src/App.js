9e4c3cc0a37722d3101b33fcc5156ae098df029d
Avi Patel <avip3513@gmail.com>
2022-09-16 08:29:54 +0530

    UseEffect

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import UseEffect from "./component/UseEffect/UseEffect";
 
 const App = () => {
-  const [values, getValues] = useState({
-    firstName: "",
-    lastName: "",
-    country: "",
-  });
-
-  // const handleChange = (e) => {
-  //   // const val = e.target.value;
-  //   // getValues(val);
-
-  // };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    getValues({
-      [name]: value,
-    });
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(values);
-  };
-
-  const { firstName, lastName, country } = values;
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          placeholder="Enter First Name"
-          value={firstName}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Enter Last Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Enter Country"
-          onChange={handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+      <UseEffect />
     </div>
   );
 };
